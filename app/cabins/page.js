e7bf19852d6aa7dbb23b749937b0bb157d2c48c8
alamin <codepapa360@gmail.com>
2024-09-06 23:15:14 +0600

    015 Adding interactivity with client Components

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,5 @@
+import Counter from "../components/Counter";
+
 export default async function Page() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
@@ -5,12 +7,12 @@ export default async function Page() {
   return (
     <div>
       <h1>Cabins page</h1>
-
       <ul>
         {data.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
+      <Counter users={data} />
     </div>
   );
 }
