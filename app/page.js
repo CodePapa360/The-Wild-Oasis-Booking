50c9b6adc685fd190e76f6d28a40296484355c2c
alamin <codepapa360@gmail.com>
2024-09-06 18:44:29 +0600

    011 Navigating etween pages

--- app/page.js
+++ app/page.js
@@ -1,3 +1,14 @@
+import Link from "next/link";
+import Navigation from "./components/Navigation";
+
 export default function Page() {
-  return <h1>The wild oasis. Welcome to paradise</h1>;
+  return (
+    <div>
+      <Navigation />
+
+      <h1>The wild oasis. Welcome to paradise</h1>
+
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  );
 }
