b3630629356e975cdc50350a966191c8932d47b2
alamin <codepapa360@gmail.com>
2024-09-06 22:56:46 +0600

    014 Fetching Data in a Page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <h1>Cabins page</h1>;
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return (
+    <div>
+      <h1>Cabins page</h1>
+
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
