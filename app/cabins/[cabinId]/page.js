df745a9f8999ca18b44712d8a6e1bac8bb293e02
alamin <codepapa360@gmail.com>
2024-09-07 21:05:52 +0600

    34-8 Generating dynamic metadata

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -2,6 +2,14 @@ import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
+export async function generateMetadata({ params }) {
+  const cabin = await getCabin(params.cabinId);
+
+  return {
+    title: `Cabin ${cabin.name}`,
+  };
+}
+
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
 
