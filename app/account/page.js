b58246cc185b1e80a85f4c5d8591d8887335cbc0
alamin <codepapa360@gmail.com>
2024-09-07 15:37:37 +0600

    33-11 Adding nested routes and pages

--- app/account/page.js
+++ app/account/page.js
@@ -3,5 +3,9 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <h1>Account page</h1>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome Alamin
+    </h2>
+  );
 }
