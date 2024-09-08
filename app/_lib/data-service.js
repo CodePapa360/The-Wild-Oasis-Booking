84e9cfb5982801efe368c6800c70223ce24c63f0
alamin <codepapa360@gmail.com>
2024-09-08 19:33:35 +0600

    45-7 Data fetching strategies for the reservation section

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -14,7 +14,7 @@ export async function getCabin(id) {
     .single();
 
   // For testing
-  // await new Promise((res) => setTimeout(res, 1000));
+  // await new Promise((res) => setTimeout(res, 2000));
 
   if (error) {
     console.error(error);
@@ -132,6 +132,9 @@ export async function getBookedDatesByCabinId(cabinId) {
 
 export async function getSettings() {
   const { data, error } = await supabase.from("settings").select("*").single();
+
+  // For testing
+  // await new Promise((res) => setTimeout(res, 5000));
 
   if (error) {
     console.error(error);
