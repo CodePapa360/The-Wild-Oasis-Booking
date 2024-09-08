8ca37d285937a21dd29698724772322e999ad529
alamin <codepapa360@gmail.com>
2024-09-08 14:33:23 +0600

    35-4 Highlighting current side navigation link

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   CalendarDaysIcon,
   HomeIcon,
@@ -5,6 +7,7 @@ import {
 } from "@heroicons/react/24/solid";
 import SignOutButton from "./SignOutButton";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const navLinks = [
   {
@@ -25,13 +28,17 @@ const navLinks = [
 ];
 
 function SideNavigation() {
+  const pathname = usePathname();
+
   return (
     <nav className="border-r border-primary-900">
       <ul className="flex flex-col gap-2 h-full text-lg">
         {navLinks.map((link) => (
           <li key={link.name}>
             <Link
-              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+              className={`${
+                pathname === link.href ? "bg-primary-900" : ""
+              } py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
               href={link.href}
             >
               {link.icon}
