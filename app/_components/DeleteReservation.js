5029a02fbf2fd0b58d2b3be432711cb2fbe1ffa4
alamin <codepapa360@gmail.com>
2024-09-10 09:16:09 +0600

    37-7 Deleting a reservation

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,10 +1,16 @@
-import { TrashIcon } from '@heroicons/react/24/solid';
+"use client";
+
+import { TrashIcon } from "@heroicons/react/24/solid";
+import { deleteReservation } from "../_lib/actions";
 
 function DeleteReservation({ bookingId }) {
   return (
-    <button className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'>
-      <TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
-      <span className='mt-1'>Delete</span>
+    <button
+      onClick={() => deleteReservation(bookingId)}
+      className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
+    >
+      <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+      <span className="mt-1">Delete</span>
     </button>
   );
 }
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
-import { updateGuest } from "./data-service";
+import { deleteBooking, getBookings, updateGuest } from "./data-service";
 
 export async function UpdateGuest(formData) {
   const session = await auth();
@@ -26,6 +26,22 @@ export async function UpdateGuest(formData) {
 
   // Revalidate the profile page to show the updated data
   revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be signed in");
+
+  /// This checking is to ensure that the user can only delete their own reservations. This is a security measure to prevent unauthorized deletion of reservations by malicious users.
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You don't have permission to delete this reservation");
+  ///
+
+  await deleteBooking(bookingId);
+  revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {
