9ccd5290c7b63bf251dc560c344bea38a8bd88e1
alamin <codepapa360@gmail.com>
2024-09-12 16:08:12 +0600

    Refactor SignInButton and login page for improved responsiveness and readability

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -1,15 +1,18 @@
+import Image from "next/image";
 import { signInAction } from "../_lib/actions";
 
 function SignInButton() {
   return (
     <form action={signInAction}>
-      <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
-        <img
-          src="https://authjs.dev/img/providers/google.svg"
-          alt="Google logo"
-          height="24"
-          width="24"
-        />
+      <button className="flex items-center gap-4 md:gap-6 text-lg border border-primary-300 px-4 py-2 md:px-10 md:py-4 font-medium">
+        <div>
+          <Image
+            src="https://authjs.dev/img/providers/google.svg"
+            alt="Google logo"
+            height="24"
+            width="24"
+          />
+        </div>
         <span>Continue with Google</span>
       </button>
     </form>
--- app/login/page.js
+++ app/login/page.js
@@ -7,7 +7,7 @@ const metadata = {
 export default function Page() {
   return (
     <div className="flex flex-col gap-10 mt-10 items-center">
-      <h2 className="text-3xl font-semibold">
+      <h2 className="text-xl text-center md:text-3xl font-semibold">
         Sign in to access your guest area
       </h2>
 
